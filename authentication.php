031041417c715fdad9d3b1f782f9dcc5fab91d95
ArinoMarino <91193643+ArinoMarino@users.noreply.github.com>
2022-05-13 09:01:47 +0200

    endret top sin filtype for testing

--- authentication.php
+++ authentication.php
@@ -16,10 +16,11 @@ echo $sql;
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
           
-        if($count == 1){  
-            include('top.php');
+        if($count == 1){
+            include 'top.html';
             echo "<h1><center> Gaming </center></h1>";  
             include 'home.html';
+            
            
         }  
         else{  
